3bcc75078ed55ca3b6b0f129c87348abb2177621
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2020-03-25 22:19:59 -0300

    Configuração inicial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+app.listen(3333, function () {
+    console.log("Servidor rodando na porta 3333");
+});
