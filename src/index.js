733d487a1bc13d5e50765e455bc0e6020bc22141
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2020-03-25 22:37:43 -0300

    Adição do ts-node-dev

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-app.listen(3333, function () {
-    console.log("Servidor rodando na porta 3333");
-});
